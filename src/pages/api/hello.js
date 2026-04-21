a0a3a0119d8f7902a9d44c096d141aaed7e22af7
Mia Antoniuk <ray.seven132@gmail.com>
2026-04-21 01:56:13 -0600

    Updated profile and added API endpoint

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import profile from "@/data/profile.json"; // adjust path if needed
 
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json(profile.about);
 }
